4f90f20666a44816bb81dee7e1e6de7f0390e72b
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-29 14:15:36 -0700

    created footer and other components

--- chef-portfolio/src/App.js
+++ chef-portfolio/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import HomePageContainer from './components/MainContainer/HomePageContainer';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePageContainer />
+      <Footer />
     </div>
   );
 }
