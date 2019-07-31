7164ce4ca2e3fad1342a587b91ef079d3a1465c8
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-31 12:52:13 -0700

    changes to home page and filters and recipesDetailed
    changes to home page and filters and recipesDetailed

--- chef-portfolio/src/components/MainContainer/HomePageContainer.js
+++ chef-portfolio/src/components/MainContainer/HomePageContainer.js
@@ -8,7 +8,6 @@ const HomePageContainer = (props) => {
     const recipesData = props.recipesData;
     return (
         <div className="HomePageWrapper">
-            <p>THIS IS MAIN BODY CONTAINER</p>
             <Header />
             <DropdownFilterSearchInMenu recipesData={recipesData} />
             <RecipesList recipesData={recipesData} />
