73bd2007f08471d8bbfdb184d16d308bedee2ec3
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-31 10:29:51 -0700

    edited recipesDetailed

--- chef-portfolio/src/components/Recipes/RecipeDetailed.js
+++ chef-portfolio/src/components/Recipes/RecipeDetailed.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Grid, Header, Image } from 'semantic-ui-react';
 const getRecipeToDisplay = (recipesData, selectedRecipe) => {
   if (recipesData.length === 0) {
     return [];
@@ -32,6 +32,11 @@ const RecipeDetailed = (props) => {
       <div className='img-and-name'>
         <img src={recipeToDisplay.imgURL} alt='food photo' />
         <h1>{recipeToDisplay.title}</h1>
+        <div className='recipeChefInfo'>
+          <img src='https://s3-alpha-sig.figma.com/img/a729/e5df/b640ff2a85dfac945ac466139ed05c59?Expires=1565568000&Signature=DjOkk94C7GgdMqYa5I4UVmzUEMFSpNkG73lNhORsGKllh9SAQKMoe4j3PrAZ0RFGUzjKaOTBdCfbPsM4u8SasGIyO7ukuozu-xCPHAEMcTRgF4v8ztIA42PA1VrrS0CXsR1w2KhxE4pGjGu9A57UW0VB2E3pq74I6WuQ9Me9R4h8FA0twTVdh8~676oUVWKkUDl~0lStdv-aIwSQpu7MRwGO3gKYvk7PPmoSefwkJs3r4DHBTGMy9NgfiQau~wRbvEASo34MVtMQ72nPfhMYInPAQY-XnHmIKmnCZvNGBiviYH~WWg5flTWyUZ24kQHEYTqH1BCk-O25hTT4qpzkyQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA' alt='${recipeToDisplay.chef_id}'/>
+          <p>{recipeToDisplay.chef_id}</p>
+
+        </div>
       </div>
       <div className='recipeInfo'>
         <div className='servings-total-time'>
@@ -44,11 +49,24 @@ const RecipeDetailed = (props) => {
         </div>
         <div className='oven-temp'>
           <p>Oven Temp: {recipeToDisplay.oven_temperature}</p>
+          <p>Meal: {recipeToDisplay.meal_type}  </p> 
         </div>
       </div>
       <div className='recipe-description'>
         <p>Description: {recipeToDisplay.description}</p>
       </div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header size='medium'>Ingredient</Header>
+            <p>{recipeToDisplay.ingredient}</p>
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Header size='medium'>Directions</Header>
+            <p>{recipeToDisplay.directions}</p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   )
 }
