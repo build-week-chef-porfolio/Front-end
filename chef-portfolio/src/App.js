fbff021f36ddd3575eac08db985ac33a19e02d75
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-30 17:15:29 -0700

    finished dynamic routing. implemented dynamic recipe detailed page.

--- chef-portfolio/src/App.js
+++ chef-portfolio/src/App.js
@@ -1,26 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
-import axios from 'axios';
-
-
 import Login from './login';
 
 //import useLocalStorage from './authorization/useLocalStorage'
 
-import HomePageContainer from './components/MainContainer/HomePageContainer';
 import Footer from './components/Footer/Footer';
-
-
 import AppRouter from './AppRouter';
-
-import RecipeCard from './components/Recipes/RecipeCard.js';
-// import RecipeDetailed from './components/Recipes/RecipeDetailed';
-// import FindChefsForm from './components/Forms/FindChefs';
-// import BrowseRecipeForm from './components/Forms/BrowseRecipe';
-import RecipesFilter from './components/Recipes/RecipesFilter';
-
-
 
 function App() {
 
