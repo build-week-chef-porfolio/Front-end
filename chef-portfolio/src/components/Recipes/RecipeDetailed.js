e2f9811786838a3aacbee0a543777b48d4bc8bda
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-31 09:03:09 -0700

    fixed image URL w/ new API link

--- chef-portfolio/src/components/Recipes/RecipeDetailed.js
+++ chef-portfolio/src/components/Recipes/RecipeDetailed.js
@@ -30,7 +30,7 @@ const RecipeDetailed = (props) => {
   return (
     <div className='recipeDetailedWrapper'>
       <div className='img-and-name'>
-        <img src='https://s3-alpha-sig.figma.com/img/63a0/c427/a8f9f5748d4674375419167bbfa4f6a3?Expires=1565568000&Signature=RvnNSgHeI82x3oBSycFoydUO~-J20c7uiE9OhfkPOXIt3aJ9SlV5EEGMQ9fRq8zE3RlTmVBkXxI80eQ0oWHbymL3Yjj4cJ-TvoV0KNWaNFbWjkbm4fB9CLXvw0BC87scEaTx58yCfrffg~g0D7kMDOjEYFJVYg~cRidIBkr~F~UolOiep4GaeTvbHiq2UNMrVRyA7N2ei9xW0utMXUFPbTpL0qZpDvCc-VyextCDivoDFad6xD~suqRHfw~afqjc4TlmJFMjzYXM2MfXOsE9B84TGm8fTFuJcS8~5yq5zs7i-khERlgpLJDmXFlsPo-aGqzp-6aIQoKQpD00r-T86w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA' alt='food photo' />
+        <img src={recipeToDisplay.imgURL} alt='food photo' />
         <h1>{recipeToDisplay.title}</h1>
       </div>
       <div className='recipeInfo'>
